d347e9157279644e56a608cc2aabdd427a93ba96
vishal.kapoor <vishal.kapoor@cgi.com>
2021-10-16 21:17:08 +0530

    Stacks using Linked list

--- stackLinkedList.py
+++ stackLinkedList.py
@@ -1,1 +1,49 @@
-stackLinkedList.py
+class Node:
+    def __init__(self, data):
+        self.root = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, data):
+        if(self.head==None):
+            self.head = Node(data)
+        else:
+            newNode = Node(data)
+            self.head.next = newNode
+        return self.head.root
+
+    def isEmpty(self):
+        if(self.head==None):
+            return True
+        return False
+    
+    def display(self):
+        arr = []
+        temp = self.head
+        while(temp):
+            arr.append(temp.root)
+            temp = temp.next
+        return arr
+
+    def pop(self):
+        temp  = self.head
+        prevNode = None
+        while(temp.next):
+            prevNode = temp
+            temp = temp.next
+        prevNode.next = None
+        return self.display()
+    
+if __name__ == '__main__':
+    stack = Stack()
+    print(stack.isEmpty())
+    print(stack.push(1))
+    print(stack.push(2))
+    print(stack.isEmpty())
+    print(stack.display())
+    print(stack.pop())
+
+    
